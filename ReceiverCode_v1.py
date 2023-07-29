b3f557dc7ae63de5bad07b0bfce75c0c6897d1d0
Mohnish4533 <mv4533@srmist.edu.in>
2023-07-29 19:06:40 +0530

    Updated Receiver Code

--- ReceiverCode_v1.py
+++ ReceiverCode_v1.py
@@ -1,15 +1,17 @@
 from pymavlink import mavutil
+from custom import custom_mavlink_1 as mavlink
 
-the_connection = mavutil.mavlink_connection('/dev/pts/2', baud=57600)
+the_connection = mavutil.mavlink_connection('/dev/pts/6', baud=57600)
+#custom_mav = mavlink.MAVLink(None)
 
 the_connection.wait_heartbeat()
 
 print("Heartbeat from system (system %u component %u)" % (the_connection.target_system, the_connection.target_component))
 
 while True:
-    try:
-        info = the_connection.messages['test_transmission']
-        timestamp = the_connection.time_since('test_transmission')
-        print("\n", info, timestamp, sep = "\t")
-    except:
-        continue
+    message = the_connection.recv_match()
+    if message != None:
+        print(message)
+    #custom_mav = mavlink.
+    #custom_msg = custom_mav.decode(message.get_payload())
+    #print(f"Received MyCustomMessage: field1={custom_msg.message_sent_1}, field2={custom_msg.message_sent_2}")    
